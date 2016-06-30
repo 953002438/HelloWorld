d327dc30fb5e64ebb50ce4af5c35ea6c5a809162
953002438 <953002438@qq.com>
2016-06-30 15:23:28 +0800

    Add files via upload

---
+++ switch.java
@@ -0,0 +1,33 @@
+class Demo
+{public static void main(String[] args)
+	{
+		int week=5;
+		switch(week)
+			{
+			case 1:
+			System.out.println(week+"对应的是星期一。");
+				break;
+			case 2:
+			System.out.println(week+"对应的是星期二。");
+				break;
+			case 3:
+			System.out.println(week+"对应的是星期三。");
+				break;
+			case 4:
+			System.out.println(week+"对应的是星期四。");
+				break;
+			case 5:
+			System.out.println(week+"对应的是星期五。");
+				break;
+			case 6:
+			System.out.println(week+"对应的是星期六。");
+				break;
+			case 7:
+			System.out.println(week+"对应的是星期天。");
+				break;
+			default:
+			System.out.println(week+"没有对应的星期。");
+				break;
+			}
+	}
+}
